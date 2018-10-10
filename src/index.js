b117667fcf795cc67995659f69428aac698133df
Eric Schubert <eric.schubert@st.ovgu.de>
2018-10-10 22:13:47 +0200

    Added Sass style build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import './styles.scss';
+
 function createConstructionShield() {
+
 	let element = document.createElement('div');
 	
 	element.innerHTML = 'Currently under construction...';
+	element.classList.add('construction-label');
 	
 	return element;
 }
 
 document.body.appendChild(createConstructionShield());
-
