5d1f24f93e621c82e55a1ffbf861ab3406cfea88
Eric Schubert <eric.schubert@st.ovgu.de>
2018-10-25 20:16:31 +0200

    Added simple styling and headlines

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
 import './styles.scss';
-
-function createConstructionShield(): HTMLElement {
-    const element = document.createElement('div');
-	
-	element.innerHTML = 'Currently under construction...';
-	element.classList.add('construction-label');
-	
-	return element;
-}
-
-document.body.appendChild(createConstructionShield());
