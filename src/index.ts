7ecfe1c732ef8d1f8dd03600061373d0b0b4137d
Eric Schubert <eric.schubert@st.ovgu.de>
2018-10-28 15:54:05 +0100

    adding HTML elements

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import './styles.scss';
 
 function createConstructionShield(): HTMLElement {
-    const element = document.createElement('div');
+	const element = document.createElement('div');
 	
 	element.innerHTML = 'Currently under construction...';
 	element.classList.add('construction-label');
-	
+
 	return element;
 }
 
 document.body.appendChild(createConstructionShield());
+
+
+function createParagraph(): HTMLElement {
+	let element = document.createElement('p');
+
+	element.innerHTML = 'beliebiger Text';
+	element.classList.add('Marie-Uebung1');
+
+	return element;
+}
+
+document.body.appendChild(createParagraph());
